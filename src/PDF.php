68b3534df437a68b4ecbd8b45262d8bbee160481
José Juan Martínez Peláez <josejuan@mpz.mx>
2022-05-28 23:34:52 -0500

    Actualización de URL de generador

--- src/PDF.php
+++ src/PDF.php
@@ -13,7 +13,7 @@ class PDF
 
     public function Generar(array $estructura)
     {
-        $url = "https://{$this->servidor}/";
+        $url = "https://{$this->servidor}/pdf";
 
         $content = json_encode($estructura);
 
