b70212d9e3bc3f55b50b4664dd85717c4cefaef0
José Juan Martínez Peláez <josejuan@mpz.mx>
2023-09-03 22:51:02 -0600

    Llamada para unir PDFs y generar desde HTML

--- src/PDF.php
+++ src/PDF.php
@@ -11,17 +11,16 @@ class PDF
 {
     private $servidor = "pdfg.sitec-mx.com";
 
-    public function Generar(array $estructura)
+    private function APICALL(string $endpoint, array $body)
     {
-        $url = "https://{$this->servidor}/generar";
+        $url = "https://{$this->servidor}/{$endpoint}";
 
-        $content = json_encode($estructura);
+        $content = json_encode($body);
 
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_HEADER, false);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HTTPHEADER,
-                array("Content-type: application/json"));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
 
@@ -30,5 +29,20 @@ class PDF
         curl_close($curl);
 
         return $result;
+    }
+
+    public function Generar(array $estructura)
+    {
+        return $this->APICALL("generar", $estructura);
+    }
+
+    public function Unir(array $estructura)
+    {
+        return $this->APICALL("unir", $estructura);
+    }
+
+    public function HTML(array $estructura)
+    {
+        return $this->APICALL("html", $estructura);
     }
 }
